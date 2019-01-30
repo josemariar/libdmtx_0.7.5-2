5c966e5ea266054aaa6f2cfb7975a3e25c100718
Jose Maria <josemaria.rico@ubotica.com>
2019-01-30 16:50:28 +0100

    script fixed

--- install_libdmtx_0.7.5-2.sh
+++ install_libdmtx_0.7.5-2.sh
@@ -6,4 +6,4 @@ sudo dpkg -i ./libdmtx-dev_0.7.5-2_amd64.deb
 sudo pip3 install pylibdmtx
 
 result=$(python3 -m site | grep "/usr/local/lib/.*/dist-packages" | tr -d ,\')
-cp ./wrapper.py $result/pylibdmtx/
+sudo cp ./wrapper.py $result/pylibdmtx/
